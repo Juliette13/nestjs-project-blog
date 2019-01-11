cfa6f1f12a84450ec7ba04dc222298404353b3cc
Juliette13 <verlaine.juliette@gmail.com>
2019-01-11 11:43:50 +0100

    Add module, controller and service for article, commentaire

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,14 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { ArticleModule } from "./article/article.module";
+import { CommentaireModule } from "./commentaire/commentaire.module";
+import { DroitModule } from "./droit/droit.module";
+import { UserModule } from "./user/user.module";
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, ArticleModule, CommentaireModule, DroitModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService]
 })
 export class AppModule {}
